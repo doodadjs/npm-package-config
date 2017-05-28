f65bff4feeb689e137fa979cf13f8d24caaa3fdb
Claude Petit <doodadjs@gmail.com>
2017-05-28 04:41:24 -0400

    Prepare for NPM 5 support + Code review + Fix project .npmrc

--- index.js
+++ index.js
@@ -25,22 +25,40 @@ THE SOFTWARE.
 
 "use strict";
 
+const path = require('path'),
+	cp = require('child_process'),
+	fs = require('fs');
+
+
 const Module = require('module').Module,
 	_require = function(_module, path) {
 		return Module._load(path, _module);
 	};
 	
-function reduceEnvironment(result) {
+
+const PACKAGE_JSON_FILE = 'package.json';
+const NPM_COMMAND = 'npm -v && npm config list';
+
+
+function splitNpmCommandResults(result) {
+	const pos = result.indexOf('\n');
+	const npmVersion = parseInt(result.slice(0, pos).trim('\r').split('.')[0]);
+	const configList = result.slice(pos + 1).trim('\r');
+	return [npmVersion, configList];
+};
+
+
+function reduceEnvironment(config) {
 	const NPM_KEY = (process.env.npm_package_name ? 'npm_package_config_' : 'npm_config_'),
 		envKeys = Object.keys(process.env);
 
-	const config = envKeys.reduce(function(result, key) {
+	envKeys.forEach(function(key) {
 		if (key.toLowerCase().startsWith(NPM_KEY)) {
-			result.env[key.slice(NPM_KEY.length)] = process.env[key];
+			config.env[key.slice(NPM_KEY.length)] = process.env[key];
 		};
-		return result;
-	}, result);
+	});
 };
+
 
 function reducePackageConfig(result, packageConfig, parent, type) {
 	const replaceRegEx = /[^A-Za-z0-9_]/g;
@@ -60,35 +78,45 @@ function reducePackageConfig(result, packageConfig, parent, type) {
 	};
 };
 
-function parse(packageName, config, fileContent, /*optional*/section) {
-	const varPrefix = packageName + ':';
+
+const GLOBAL_SECTION_NAME = "; globalconfig ";
+const USER_SECTION_NAME = "; userconfig ";
+const PROJECT_SECTION_NAME = "; project config "; // npm v5
+
+function parse(npmVersion, packageName, config, fileContent, /*optional*/section) {
+	const varPrefix = (packageName ? packageName + ':' : '');
 	let currentSection = section;
-	return fileContent.split('\n').filter(function(line) {
-			return line.startsWith("; globalconfig ") || line.startsWith("; userconfig ") || (line.startsWith(varPrefix));
-		}).reduce(function(result, line) {
-			if (!section && line.startsWith("; globalconfig ")) {
+	fileContent.split('\n').filter(function(line) {
+			return line.trim('\r').startsWith(GLOBAL_SECTION_NAME) || line.startsWith(USER_SECTION_NAME) || ((npmVersion >= 5) && line.startsWith(PROJECT_SECTION_NAME)) || !varPrefix || line.startsWith(varPrefix);
+		}).forEach(function(line) {
+			if (!section && line.startsWith(GLOBAL_SECTION_NAME)) {
 				currentSection = 'global';
-			} else if (!section && line.startsWith("; userconfig ")) {
+			} else if (!section && line.startsWith(USER_SECTION_NAME)) {
 				currentSection = 'user';
+			} else if (!section && (npmVersion >= 5) && line.startsWith(PROJECT_SECTION_NAME)) {
+				currentSection = 'project';
 			} else if (currentSection) {
-				line = line.trim('\r').slice(varPrefix.length).split('=', 2);
-				const key = line[0].trim();
+				if (varPrefix) {
+					line = line.slice(varPrefix.length);
+				};
+				const pos = line.indexOf('=');
+				const key = line.slice(0, pos).trim();
 				if (key) {
-					let val = line[1].trim();
+					let val = line.slice(pos + 1).trim();
 					if (val.startsWith('"')) {
-						//console.log(val);
 						val = JSON.parse(val);
 					};
-					result[currentSection][key] = val;
+					config[currentSection][key] = val;
 				};
 			};
-			return result;
-		}, config);
+		});
 };
 
-function combine(config) {
+
+function combine(npmVersion, config) {
 	return Object.assign({}, config.package, config.global, config.user, config.project, config.env);
 };
+
 
 function beautify(config) {
 	return Object.keys(config).reduce(function(result, key) {
@@ -129,6 +157,8 @@ function beautify(config) {
 	}, {});
 };
 
+
+
 module.exports = {
 	list: function list(/*optional*/packageName, /*optional*/options) {
 		options = options || {};
@@ -141,73 +171,103 @@ module.exports = {
 			env: {}, 
 		};
 		
-
-		let parent = module.parent,
-			prevParent = null,
+		let mainModule = module.parent,
 			_package = null,
-			_module = null;
-		while (parent && (parent.id !== 'repl') && (parent !== prevParent)) {
-			prevParent = parent;
-			try {
-				_package = _require(parent, './package.json');
-				_module = parent;
-				break;
-			} catch(ex) {
-				parent = parent.parent;
+			upDir = '';
+
+		const packageJson = '.' + path.sep + PACKAGE_JSON_FILE;
+		const mainPath = path.dirname(mainModule.filename);
+
+		while (mainModule && (mainModule.id !== 'repl')) {
+			const pathsLen = mainModule.paths.length + 1;
+
+			for (let i = 0; i < pathsLen; i++) {
+				try {
+					_package = JSON.parse(fs.readFileSync(mainPath + path.sep + upDir + packageJson));
+					break;
+				} catch(ex) {
+					if (ex.code !== 'MODULE_NOT_FOUND') {
+						throw ex;
+					};
+
+					upDir += '..' + path.sep;
+				};
 			};
+
+			if (_package) {
+				break;
+			};
+
+			mainModule = mainModule.parent;
 		};
-		if (!_package) {
-			_module = module;
-			_package = _require(_module, './package.json');
+
+		if (!mainModule) {
+			mainModule = require.main || module;
 		};
-		
-		if (!packageName || (packageName === _package.name)) {
-			packageName = _package.name;
+
+		let isMain = false;
+		if (!packageName || (_package && (packageName === _package.name))) {
 			reduceEnvironment(config);
-		} else {
-			_package = _require(_module, packageName + '/package.json');
+			isMain = true;
+		};
+
+		if (packageName && !isMain) {
+			_package = _require(mainModule, packageName + path.sep + PACKAGE_JSON_FILE);
+		} else if (!_package) {
+			throw new Error("No '" + PACKAGE_JSON_FILE + "' found.");
 		};
 
 		reducePackageConfig(config, _package, 'package_', 'package');
-		reducePackageConfig(config, _package.config, '_', 'config');
+		reducePackageConfig(config, _package.config, '', 'config');
 		
-		const path = require('path'),
-			cp = require('child_process'),
-			fs = require('fs');
-			
-		const packageFolder = path.dirname(_module.filename);
-			
+		const packageFolder = mainPath + path.sep + upDir;
+		
 		if (options.async) {
-			function listNpm() {
+			function listNpm(config) {
 				return new Promise(function(resolve, reject) {
-					cp.exec('npm config list', {encoding: 'utf-8', cwd: packageFolder}, function(err, fileContent) {
+					cp.exec(NPM_COMMAND, {encoding: 'utf-8', cwd: packageFolder}, function(err, stdout) {
 						if (err) {
 							reject(err);
 						} else {
-							resolve(parse(packageName, config, fileContent));
+							const retval = splitNpmCommandResults(stdout);
+							const npmVersion = retval[0];
+							const configList = retval[1];
+							parse(npmVersion, packageName || _package.name, config, configList);
+							resolve([npmVersion, config]);
 						};
 					});
 				});
 			};
-			function listProject() {
-				return new Promise(function(resolve, reject) {
-					fs.readFile(packageFolder + '/.npmrc', {encoding: 'utf-8'}, function(err, fileContent) {
-						if (err) {
-							if (err.code !== 'ENOENT') {
-								reject(err);
+			function listProject(args) {
+				const npmVersion = args[0];
+				const config = args[1];
+				if (npmVersion < 5) {
+					return new Promise(function(resolve, reject) {
+						fs.readFile(packageFolder + '.npmrc', {encoding: 'utf-8'}, function(err, fileContent) {
+							if (err) {
+								if (err.code === 'ENOENT') {
+									resolve(args);
+								} else {
+									reject(err);
+								};
 							} else {
-								resolve(config);
+								parse(npmVersion, packageName, config, fileContent, 'project');
+								resolve([npmVersion, config]);
 							};
-						} else {
-							resolve(parse(packageName, config, fileContent, 'project'));
-						};
+						});
 					});
-				});
+				} else {
+					resolve(args);
+				};
 			};
 			
-			return listNpm()
+			return listNpm(config)
 				.then(listProject)
-				.then(combine)
+				.then(function(args) {
+					const npmVersion = args[0];
+					const config = args[1];
+					return combine(npmVersion, config);
+				})
 				.then(function(result) {
 					if (options.beautify) {
 						return beautify(result);
@@ -216,16 +276,20 @@ module.exports = {
 					};
 				});
 		} else {
-			//"Error: invalid data"   cp.execFileSync('npm', ['config', 'list'], {encoding: 'utf-8', cwd: packageFolder});
-			parse(packageName, config, cp.execSync('npm config list', {encoding: 'utf-8', cwd: packageFolder}));
-			try {
-				parse(packageName, config, fs.readFileSync(packageFolder + '/.npmrc', {encoding: 'utf-8'}), 'project');
-			} catch(ex) {
-				if (ex.code !== 'ENOENT') {
-					throw ex;
+			let retval = splitNpmCommandResults(cp.execSync(NPM_COMMAND, {encoding: 'utf-8', cwd: packageFolder}));
+			const npmVersion = retval[0];
+			const configList = retval[1];
+			parse(npmVersion, packageName || _package.name, config, configList);
+			if (npmVersion < 5) {
+				try {
+					parse(npmVersion, packageName, config, fs.readFileSync(packageFolder + '.npmrc', {encoding: 'utf-8'}), 'project');
+				} catch(ex) {
+					if (ex.code !== 'ENOENT') {
+						throw ex;
+					};
 				};
 			};
-			let result = combine(config);
+			let result = combine(npmVersion, config);
 			if (options.beautify) {
 				result = beautify(result);
 			};
